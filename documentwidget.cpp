c3aeec98e9cc4cb4832dc3cdd491081daba71ee8
VictorFung1 <victor.fung122@gmail.com>
2015-12-01 22:11:06 -0500

    Added read/close functions to the doucment widget

--- documentwidget.cpp
+++ documentwidget.cpp
@@ -1,7 +1,53 @@
 #include "documentwidget.h"
 
+bool open = false; //Variable to keep track if a document is open
+              //false if no document open, true if document is open
 DocumentWidget::DocumentWidget(QWidget *parent) : QWidget(parent)
 {
-
+    createLayouts(); //Create layout within Document Tab
+    if (open == true)
+        closeFile();
+    if (open == false)
+        readFile();
 }
 
+//Layout for the Document Tab
+void DocumentWidget::createLayouts()
+{
+    QWidget* widget = new QWidget();
+    m_mainLayout = new QVBoxLayout(widget);
+    txt = new QTextEdit(widget); //Create a Text Box Widget
+    m_mainLayout->addWidget(txt); //Place the Text Box Widget into the main layout
+    setLayout(m_mainLayout);
+}
+
+//read the file
+void DocumentWidget::readFile()
+{
+    qDebug() << "Reading try.txt";
+    open = true; //indicate there a file currently open
+    QFile file("/home/vfung000/Desktop/try.txt");
+    QString line;
+
+    //handle error
+    if(!file.open(QIODevice::ReadOnly))
+    {
+        QMessageBox::information(0, "error", file.errorString());
+    }
+
+    QTextStream in(&file); //read into file
+    while(!in.atEnd())
+    {
+        line = in.readLine();
+        txt->append(line);
+    }
+    file.close(); //close the file
+}
+
+//close the (current) file
+void DocumentWidget::closeFile()
+{
+    qDebug() << "Closing the current file";
+    txt->clear();
+    open = false;
+}
--- documentwidget.h
+++ documentwidget.h
@@ -2,6 +2,13 @@
 #define DOCUMENTWIDGET_H
 
 #include <QWidget>
+#include <QVBoxLayout>
+#include <QTextEdit>
+#include <QLineEdit>
+#include <QFile>
+#include <QMessageBox>
+#include <QTextStream>
+#include <QDebug>
 
 class DocumentWidget : public QWidget
 {
@@ -12,6 +19,14 @@ public:
 signals:
 
 public slots:
+
+private:
+    QTextEdit *txt;
+    QVBoxLayout *m_mainLayout;
+    void createLayouts();
+
+    void closeFile();
+    void readFile();
 };
 
 #endif // DOCUMENTWIDGET_H
